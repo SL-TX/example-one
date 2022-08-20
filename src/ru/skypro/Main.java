af9fdd2a39fca564dd74cd4c863b3f1f046c835a
Вячеслав Тихонов <sl-tx@yandex.ru>
2022-08-21 00:11:28 +0300

    Коммит ДЗ

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,7 +1,20 @@
 package ru.skypro;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+//        Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + ' ' + lastName + ' ' + middleName;
+        System.out.println(fullName);
+
+//        Задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+//        Задание 3
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
 
     }
 }
