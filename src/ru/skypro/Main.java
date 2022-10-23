a62f95ad723b5556e57bf300fbb0191d87fba62c
Вячеслав Тихонов <sl-tx@yandex.ru>
2022-10-23 14:24:47 +0300

    part 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,7 +1,19 @@
 package ru.skypro;
 
+import ru.skypro.exceptions.WrongLoginException;
+import ru.skypro.exceptions.WrongPasswordException;
+
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args){
-
+        String login = "1234", pass="43251", cpass="4321";
+        try {
+            NewClass.method(login,pass,cpass);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Good");
+        }
     }
 }
