2467c2e0a33a56a7cfd59ab0a0bc949624122b0e
Вячеслав Тихонов <sl-tx@yandex.ru>
2022-10-26 16:23:17 +0300

    fixes

--- src/ru/skypro/products/Recept.java
+++ src/ru/skypro/products/Recept.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class Recept {
 
-    Map<Tovar,Integer> MnozhestvoProductov = new HashMap<>();
+    Map<Tovar,Integer> mnozhestvoProductov = new HashMap<>();
     private final String name;
-    public Integer SumCostTovar(){
+    public Integer sumCostTovar(){
         int i=0;
-        for (Map.Entry<Tovar,Integer> tovar: MnozhestvoProductov.entrySet()) {
+        for (Map.Entry<Tovar,Integer> tovar: mnozhestvoProductov.entrySet()) {
 
             i+=tovar.getKey().getCost()*tovar.getValue();
         }
@@ -22,9 +22,9 @@ public class Recept {
     @Override
     public String toString() {
         return "Рецепт{" +
-                "МножествоПродуктов=" + MnozhestvoProductov +
+                "МножествоПродуктов=" + mnozhestvoProductov +
                 ", Название='" + name + '\'' +
-                ", СуммарнаяСтоимостьПродуктов='" + SumCostTovar() + '\'' +
+                ", СуммарнаяСтоимостьПродуктов='" + sumCostTovar() + '\'' +
                 '}';
     }
 
@@ -32,21 +32,21 @@ public class Recept {
         if (count<1){
             throw new RuntimeException("Количество продукта в рецепте не может быть меньше 1.");
         }
-        MnozhestvoProductov.put(val,count);
+        mnozhestvoProductov.put(val,count);
     }
     public void remFromSet(Tovar val){
-        MnozhestvoProductov.remove(val);
+        mnozhestvoProductov.remove(val);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Recept recept)) return false;
-        return Objects.equals(MnozhestvoProductov, recept.MnozhestvoProductov) && Objects.equals(name, recept.name);
+        return Objects.equals(mnozhestvoProductov, recept.mnozhestvoProductov) && Objects.equals(name, recept.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(MnozhestvoProductov, name);
+        return Objects.hash(mnozhestvoProductov, name);
     }
 }
